de8707c9973ba277d5c183395f142c605b54995c
AmanGitkumar <kumaraman55475@gmail.com>
2025-03-05 12:34:27 +0530

    change frontend url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 8080;
 // ✅ Middleware Setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); // ✅ Allow frontend requests
+app.use(cors({ origin: "https://f1-euwb.onrender.com", credentials: true })); // ✅ Allow frontend requests
 app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
